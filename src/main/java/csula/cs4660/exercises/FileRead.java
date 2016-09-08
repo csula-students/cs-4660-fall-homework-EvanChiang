753e3305eef2211528a7338355fcce91722083cf
Evan Chiang <echiang3@csula.edu>
2016-09-08 11:31:56 -0700

    Exercise 1

--- src/main/java/csula/cs4660/exercises/FileRead.java
+++ src/main/java/csula/cs4660/exercises/FileRead.java
@@ -1,6 +1,8 @@
 package csula.cs4660.exercises;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Introduction Java exercise to read file
@@ -11,8 +13,38 @@ public class FileRead {
      * Read the file and store the content to 2d array of int
      * @param file read file
      */
-    public FileRead(File file) {
-        // TODO: read the file content and store content into numbers
+
+    public FileRead(File file) throws FileNotFoundException, IOException{
+        numbers = new int[5][8];
+        int index = 0;
+        int row = 0;
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line = br.readLine();
+        while (line != null)
+        {
+            String elements[] = line.split(" ");
+            for (int j = 0; j < elements.length; j++)
+            {
+                int i = Integer.parseInt(elements[j]);
+                numbers[row][index] = i;
+                index++;
+            }
+            row++;
+            index = 0;
+            line = br.readLine();
+        }
+        int greatestLine = 0;
+        int greatestLineMean = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("line " + i + " has sum " + sum(i));
+            int mean = mean(i);
+            if (mean > greatestLineMean)
+            {
+                greatestLine = i;
+                greatestLineMean = mean;
+            }
+        }
+        System.out.println("line " + greatestLine + " has the greatest mean of " + greatestLineMean);
     }
 
     /**
@@ -23,18 +55,44 @@ public class FileRead {
      * lineNumber starts with 0 (programming friendly!)
      */
     public int mean(int lineNumber) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < numbers[lineNumber].length; i ++)
+        {
+            sum += numbers[lineNumber][i];
+        }
+        return sum / numbers[lineNumber].length;
     }
 
     public int max(int lineNumber) {
-        return 0;
+        int max = 0;
+        for (int i = 0; i < numbers[lineNumber].length; i ++)
+        {
+            if (numbers[lineNumber][i] > max)
+            {
+                max = numbers[lineNumber][i];
+            }
+        }
+        return max;
     }
 
     public int min(int lineNumber) {
-        return 0;
+        int min = 0;
+        for (int i = 0; i < numbers[lineNumber].length; i ++)
+        {
+            if (numbers[lineNumber][i] < min)
+            {
+                min = numbers[lineNumber][i];
+            }
+        }
+        return min;
     }
 
     public int sum(int lineNumber) {
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < numbers[lineNumber].length; i ++)
+        {
+            sum += numbers[lineNumber][i];
+        }
+        return sum;
     }
 }

891973616951f75083fc70bc4ed2b39987582576
Evan Chiang <echiang3@csula.edu>
2016-10-01 10:23:55 -0700

    hw2 changes

--- src/main/java/csula/cs4660/graphs/searches/BFS.java
+++ src/main/java/csula/cs4660/graphs/searches/BFS.java
@@ -4,6 +4,9 @@ import csula.cs4660.graphs.Edge;
 import csula.cs4660.graphs.Graph;
 import csula.cs4660.graphs.Node;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,7 +14,38 @@ import java.util.List;
  */
 public class BFS implements SearchStrategy {
     @Override
+
     public List<Edge> search(Graph graph, Node source, Node dist) {
-        return null;
+        ArrayList<Edge> returnList = new ArrayList<Edge>();
+        LinkedList<Node> searchQueue = new LinkedList<Node>();
+        HashMap<Node, Node> parentMap = new HashMap<>();
+
+        searchQueue.add(source);
+        while (!searchQueue.isEmpty())
+        {
+            Node currentNode = searchQueue.pop();
+            if (!parentMap.containsKey(currentNode))
+            {
+                for (Node n: graph.neighbors(currentNode))
+                {
+                    parentMap.put(currentNode, n);
+                    searchQueue.add(n);
+                }
+                if (currentNode.equals(dist))
+                {
+                    Node child = dist;
+                    Node parent = parentMap.get(child);
+                    while (!child.equals(source))
+                    {
+                        returnList.add(new Edge(parent, child, graph.distance(parent, child)));
+                        child = parent;
+                        parent = parentMap.get(child);
+                    }
+                    return returnList;
+                }
+            }
+        }
+
+        return returnList;
     }
 }

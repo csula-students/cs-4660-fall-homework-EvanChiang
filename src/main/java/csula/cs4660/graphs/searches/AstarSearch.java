8c83132b3876359ce6df82dcdf5f557cb8f7b6fc
Evan Chiang <echiang3@csula.edu>
2016-10-15 18:08:53 -0700

    fixed merging issue in astarsearch

--- src/main/java/csula/cs4660/graphs/searches/AstarSearch.java
+++ src/main/java/csula/cs4660/graphs/searches/AstarSearch.java
@@ -16,54 +16,7 @@ public class AstarSearch implements SearchStrategy {
 
     @Override
     public List<Edge> search(Graph graph, Node source, Node dist) {
-        ArrayList<Edge> returnList = new ArrayList<Edge>();
-        LinkedList<Node> searchQueue = new LinkedList<Node>();
-        HashMap<Node, Node> parentMap = new HashMap<>(); //key is child, value is parent
-        HashMap<Node, Integer> distanceMap = new HashMap<>(); //the key's distance away from source
-        searchQueue.add(source);
-        distanceMap.put(source, 0);
-        System.out.println(graph.neighbors(source).toString());
-
-        while (!searchQueue.isEmpty())
-        {
-            Node currentNode = searchQueue.pop();
-            System.out.println(currentNode.toString());
-            for (Node n: graph.neighbors(currentNode))
-            {
-                System.out.println("test");
-                int distance = graph.distance(currentNode, n) + distanceMap.get(currentNode)
-                        + heuristic(graph, currentNode, dist);
-
-                if (parentMap.containsKey(n))
-                {
-                    if (distanceMap.get(n) > distance)
-                    {
-                        parentMap.put(n, currentNode);
-                        distanceMap.put(n, distance);
-                    }
-                }
-                else
-                {
-                    parentMap.put(n, currentNode);
-                    distanceMap.put(n, distance);
-                    searchQueue.add(n);
-                }
-            }
-        }
-        Node child = dist;
-        Node parent = parentMap.get(child);
-        while (!child.equals(source))
-        {
-            returnList.add(0, new Edge(parent, child, graph.distance(parent, child)));
-            child = parent;
-            parent = parentMap.get(child);
-        }
-        return returnList;
+        return null;
     }
 
-    private int heuristic(Graph graph, Node start, Node dest)
-    {
-
-        return 0;
-    }
 }
